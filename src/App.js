8d04b9d66f71585412d52dd99d19deec605a1e17
Preshy30 <121048011+Preshy30@users.noreply.github.com>
2022-12-28 09:27:48 +0100

    Update to the App.js file

--- src/App.js
+++ src/App.js
@@ -13,10 +13,18 @@ import {
   Scatter,
   ScatterChart,
   LineChart,
+  PieChart,
+  Pie,
+  PolarAngleAxis,
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarRadiusAxis,
 } from "recharts";
 import "./App.css";
 
 // Add some mock data
+
 const data = [
   {
     month: 'January',
@@ -48,63 +56,117 @@ const data = [
     savings: 9000,
     loss: 1200
   },
-]
+];
 
 function App() {
-  return <div className="App">
-   <h1>Simple Line chart</h1>
-   <LineChart width={1000} height={300} data={data}>
-    <CartesianGrid></CartesianGrid>
-    <XAxis dataKey="month"></XAxis>
-    <YAxis></YAxis>
-    <Tooltip> </Tooltip>
-    <Legend></Legend>
-    <Line type="monotone" dataKey="loss" stroke="green" />
-    <Line type="monotone" dataKey="savings" stroke="red"/>
-   </LineChart>
+  return (
+    <div className="App">
+      <h1>Simple Line chart</h1>
+      <LineChart width={1000} height={300} data={data}>
+        <CartesianGrid></CartesianGrid>
+        <XAxis dataKey="month"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip> </Tooltip>
+        <Legend></Legend>
+        <Line type="monotone" dataKey="loss" stroke="green" />
+        <Line type="monotone" dataKey="savings" stroke="red" />
+      </LineChart>
 
-   <h1>Simple Area Chart</h1>
-   <AreaChart width={1000} height={300} data={data}>
-    <CartesianGrid></CartesianGrid>
-    <XAxis dataKey="month"></XAxis>
-    <YAxis></YAxis>
-    <Tooltip> </Tooltip>
-    <Legend></Legend>
-    <Area type="monotone" dataKey="savings" stroke="blue" fill="blue"/>
-    <Area type="monotone" dataKey="loss" stroke="green" fill="green"  />
-    </AreaChart>
+      <h1>Simple Area Chart</h1>
+      <AreaChart width={1000} height={300} data={data}>
+        <CartesianGrid></CartesianGrid>
+        <XAxis dataKey="month"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip> </Tooltip>
+        <Legend></Legend>
+        <Area type="monotone" dataKey="savings" stroke="blue" fill="blue" />
+        <Area type="monotone" dataKey="loss" stroke="green" fill="green" />
+      </AreaChart>
 
-    <h1>Simple Bar Chart</h1>
-    <BarChart width={1000} height={300} data={data}>
-    <CartesianGrid></CartesianGrid>
-    <XAxis dataKey="month"></XAxis>
-    <YAxis></YAxis>
-    <Tooltip> </Tooltip>
-    <Legend></Legend>
-    <Bar type="monotone" dataKey="savings" fill="blue"/>
-    <Bar type="monotone" dataKey="loss" fill="green"  />
-   </BarChart>
+      <h1>Simple Bar Chart</h1>
+      <BarChart width={1000} height={300} data={data}>
+        <CartesianGrid></CartesianGrid>
+        <XAxis dataKey="month"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip> </Tooltip>
+        <Legend></Legend>
+        <Bar type="monotone" dataKey="savings" fill="blue" />
+        <Bar type="monotone" dataKey="loss" fill="green" />
+      </BarChart>
 
-   <h1>Stacked Bar Charts</h1>
-   <BarChart width={1000} height={300} data={data}>
-    <CartesianGrid></CartesianGrid>
-    <XAxis dataKey="month"></XAxis>
-    <YAxis></YAxis>
-    <Tooltip> </Tooltip>
-    <Legend></Legend>
-    <Bar type="monotone" dataKey="savings"  stackId="a" fill="green"/>
-    <Bar type="monotone" dataKey="loss" stackId="a" fill="red"  />
-   </BarChart>
+      <h1>Stacked Bar Charts</h1>
+      <BarChart width={1000} height={300} data={data}>
+        <CartesianGrid></CartesianGrid>
+        <XAxis dataKey="month"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip> </Tooltip>
+        <Legend></Legend>
+        <Bar type="monotone" dataKey="savings" stackId="a" fill="green" />
+        <Bar type="monotone" dataKey="loss" stackId="a" fill="red" />
+      </BarChart>
 
-   <h1>Simple Scatterplot</h1>
-   <ScatterChart width={1000} height={300} >
-    <CartesianGrid></CartesianGrid>
-    <XAxis type="category" dataKey="month" name="month"></XAxis>
-    <YAxis type="number" dataKey="savings" name="savings"></YAxis>
-    <Tooltip> </Tooltip>
-    <Scatter data={data} fill="red"></Scatter>
-   </ScatterChart>
-  </div>
+      <h1>Simple Scatterplot</h1>
+      <ScatterChart
+        width={1000}
+        height={300}
+        margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis type="category" dataKey="month" name="month"></XAxis>
+        <YAxis type="number" dataKey="savings" name="savings"></YAxis>
+        <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+        <Scatter data={data} fill="green"></Scatter>
+      </ScatterChart>
+
+      <h1>Pie Chart</h1>
+      <PieChart width={1000} height={300}>
+        <Pie
+          data={data}
+          dataKey="savings"
+          nameKey="month"
+          name="savings"
+          cx="50%"
+          cy="50%"
+          outerRadius={50}
+          fill="#8884d8"
+        />
+        <Pie
+          data={data}
+          dataKey="loss"
+          nameKey="month"
+          name="loss"
+          cx="50%"
+          cy="50%"
+          innerRadius={60}
+          outerRadius={80}
+          fill="#82ca9d"
+          label
+        />
+      </PieChart>
+
+      <h1>RadarChart</h1>
+      <RadarChart outerRadius={90} width={1000} height={300} data={data}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="month" />
+        <PolarRadiusAxis angle={30} domain={[0, 150]} />
+        <Legend />
+        <Radar
+          name="savings"
+          dataKey="savings"
+          stroke="#8884d8"
+          fill="#8884d8"
+          fillOpacity={0.6}
+        />
+        <Radar
+          name="loss"
+          dataKey="loss"
+          stroke="#82ca9d"
+          fill="#82ca9d"
+          fillOpacity={0.6}
+        />
+      </RadarChart>
+    </div>
+  );
 }
 
 export default App;
